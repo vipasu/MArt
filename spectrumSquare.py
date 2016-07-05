36f901222b46404aadf1baa4760d2b107632bb7a
Vincent Su <phazedphantom@gmail.com>
2016-07-04 17:16:37 -0700

    Added color palette

---
+++ spectrumSquare.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from math import random
+
+
+colors = ['cyan', 'goldenrod', 'seafoam green', 'light yellow', 'scarlet',
+        'neon blue', 'barney purple', 'reddish orange', 'lemon', 'cerise',
+        'light lime green', 'teal blue', 'bubblegum pink', 'black',
+        'vermillion', 'amber', 'melon', 'purpleish', 'bright light blue',
+        'strawberry', 'celadon']
+palette = [sns.xkcd_rgb[c] for c in colors]
+
+def generate_random_color():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
