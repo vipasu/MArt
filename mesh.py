42f01632443ee0e3a484766e813196a01ecb70be
Vincent Su <phazedphantom@gmail.com>
2016-07-10 16:17:52 -0700

    Extended mesh.py to allow for connections across different axes

--- mesh.py
+++ mesh.py
@@ -11,12 +11,14 @@ rc('text', usetex=True)
 N = 100
 numcols = 20
 
-def generate_points(n):
-    xs = np.linspace(1./n,1,n)
-    ys = xs[::-1]
-    return zip(zip(xs,np.zeros(n)), zip(np.zeros(n), ys))
+def generate_points(n, xfill=0, yfill=0, xreverse=False, yreverse=True):
+    xs, ys = np.linspace(0,1,n), np.linspace(0,1,n)
+    if xreverse:
+        xs = xs[::-1]
+    if yreverse:
+        ys = ys[::-1]
+    return zip(zip(xs,np.ones(n)*xfill), zip(np.ones(n)*yfill, ys))
 
-segments = generate_points(N)
 fig = plt.figure(figsize=(10,10))
 fig.patch.set_visible(False)
 ax = plt.gca()
@@ -27,8 +29,18 @@ ax.set_xticks([])
 ax.set_yticks([])
 
 
-palette = sns.color_palette('husl', numcols)
-for i, segment in enumerate(segments):
-    plt.plot(segment[0], segment[1], color=palette[i%numcols], alpha=0.5)
+def plot_segments(segments):
+    palette = sns.color_palette('husl', numcols)
+    for i, segment in enumerate(segments):
+        plt.plot(segment[0], segment[1], color=palette[i%numcols], alpha=0.5)
+
+lower_left_mesh = generate_points(N, yreverse=True)
+plot_segments(lower_left_mesh)
+upper_left_shade = generate_points(N, yfill=1)
+plot_segments(upper_left_shade)
+upper_right_mesh = generate_points(N, xfill=1, yfill=1, yreverse=True)
+plot_segments(upper_right_mesh)
+lower_right_shade = generate_points(N, xfill=1)
+plot_segments(lower_right_shade)
 
 plt.show()
