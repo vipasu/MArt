70dd4331237c3c48861fc9a7e670000610403962
Vincent Su <vipasu@stanford.edu>
2016-08-20 15:26:02 -0700

    Finished implementing clock.py

--- clock.py
+++ clock.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
+import plotting as p
+
 
 def get_time():
-    d = datetime.now()  # .gethours, minute
+    d = datetime.now()
     return d.hour, d.minute
 
 
@@ -10,51 +13,72 @@ def angle_from_time(hour, minute):
     """
     Returns the angles for both hand and minute
     """
-    theta_m = minute/float(60)
-    hour_angle = 2 * np.pi / 12
+    # Angles should be scaled fractions of the full circle
+    full_circle = 2 * np.pi
+    hour_angle = full_circle / float(12)
+
+    minute_fraction = minute / float(60)
+    hour_fraction = (hour % 12) / float(12)
+
+    theta_m =  minute_fraction * full_circle
     # Adjust for the position of the minute hand
-    theta_h = (hour % 12)/float(12) + theta_m/ (2 * np.pi) * hour_angle
+    theta_h = hour_fraction * full_circle + minute_fraction * hour_angle
 
     def rotate_to_x_axis(theta):
         """
         Converts angles specified from y axis to ones from x axis
         """
-        return np.pi - theta
+        return np.pi/2 - theta
 
     return rotate_to_x_axis(theta_h), rotate_to_x_axis(theta_m)
 
 
-def plot_clock_face():
+def plot_clock_face(col='w'):
     """
-    Should print just a white circle on black background.
-    Possibly add a hint of color (solid or colorwheel)
-    Maybe a dot where the 12 should be.
+    Returns polar axes with a solid ring.
+
+    Optionally specify the color of the ring
     """
-    pass
+    p.remove_axes()
+    fig = plt.figure(figsize=(10,10), facecolor='k')
+    ax = fig.add_subplot(111, polar=True)
+    n_points = 100
+    ax.plot(np.linspace(0, 2 * np.pi, n_points), n_points * [0.95], color=col,
+            alpha=0.3)
+    return ax
 
 
-def plot_hands(theta_h, theta_m):
-    hand_lengths = [0.7, 1]
-    center = (0, 0)
+def plot_hands(theta_h, theta_m, ax, color='w'):
+    hand_lengths = [0.5, 0.8]
     for theta, r in zip([theta_h, theta_m], hand_lengths):
-        x, y = r * np.cos(theta), r * np.sin(theta)
-        plt.plot([0, x], [0, y], 'w')
+        ax.plot([0, theta], [0, r], color)
+    return
 
 
-def generate_sweep(theta_i, theta_f):
+def generate_sweep(theta_i, theta_f, ax, color='w'):
     """
-    Creates a gradient of white to gray such that theta_f is white.
+    Creates a gradient of opacity such that theta_f is opaque.
     Accepts:
         - theta_i - initial angle
         - theta_f - final angle
     """
 
-    pass
+    rmin, rmax = 0, 0.8
+    nslices = 200
+    angles = np.linspace(theta_i, theta_f, nslices)
+    alphas = np.linspace(0, 1, nslices)**2
+    for i in xrange(nslices-1):
+        ax.fill_between(angles[i:i+2], 2 * [rmin], 2 * [rmax], alpha=alphas[i], color=color)
+    return ax
+
 
 def main():
     h, m = get_time()
     h_theta, m_theta = angle_from_time(h, m)
-    plot_hands(h_theta, m_theta)
+    ax = plot_clock_face(p.sns.xkcd_rgb['teal'])
+    plot_hands(h_theta, m_theta, ax)
     delta_t = np.random.uniform(0, 2 * np.pi)
-    generate_sweep(m_theta - delta_t, m_theta)
 
+    # adding an angle is going counter clockwise (backwards in time)
+    generate_sweep(m_theta + delta_t, m_theta, ax)
+    p.remove_axes()
